24e6f4105761128f788b57d9c1aac34fab6b2f88
Sanvi8081 <sanviporwal3101@gmail.com>
2026-02-06 22:30:27 +0530

    Add files via upload
    Day 1: Implement variables and data types examples in Python
    
    - Demonstrated usage of int, float, string, and boolean data types
    - Added sample code showing variable assignment and type checking
    - Included basic print statements for output verification

---
+++ variables_and_datatypes.py
@@ -0,0 +1,19 @@
+# DAY 1 variables and data types in python
+print("STUDENT INFO.")
+#integer 
+roll_number=57
+print("1.ROLL NO:",roll_number)
+print(type(roll_number))
+#float
+cgpa=8.71
+print("2.CGPA:",cgpa)
+print(type(cgpa))
+#string
+name="Aahan" 
+print("3.STUDENT NAME:",name)
+print(type(name))
+#Boolean
+passed=True
+print("4.IS PASSED",passed)
+print(type(passed))
+
