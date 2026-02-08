f5b106644f9693d0ff737a4d08093ebd7a3eb640
Sanvi8081 <sanviporwal3101@gmail.com>
2026-02-08 18:43:57 +0530

    Add files via upload

---
+++ operators_and_typeconversion.py
@@ -0,0 +1,48 @@
+#types of operators
+#arithmetic operators
+a=5
+b=2
+sum=a+b
+diff=a-b
+multi=a*b
+div=a/b
+modulo=a%b #remainder
+pow=a**b #a^b
+print(sum , diff , multi , div , modulo , pow)
+
+#relational operators
+a=50
+b=60
+print( a==b , a!=b , a>b , a<b , a>=b, a<=b)
+
+#assignment operators
+num=5
+num+=10
+print(num)
+num-=10
+print(num)
+num*=10
+print(num)
+num/=10
+print(num)
+num%=10
+print(num)
+num**=10
+print(num)
+
+#logical operators
+a=50
+b=30
+print(not False)
+print(not (a>b))
+val1=True
+val2=False
+print("AND op", val1 and val2)
+print("OR op", val1 or val2)
+
+#type conversion
+a=float("2")
+b=4.5
+print(type(a))
+print(a+b)
+
---
+++ sum_and_area.py
@@ -0,0 +1,10 @@
+#program to input 2 numbers and print their sum
+num1=int(input("ENTER THE FIRST NUMBER:"))
+num2=int(input("ENTER THE SECOND NUMBER:"))
+sum=num1+num2
+print("SUM OF 2 NUMBERS IS:", sum)
+
+#program to input side of a square and print its area
+side=float(input("ENTER THE SIDE OF THE SQUARE:"))
+area=side*side
+print("AREA OF A SQUARE IS:",area)
