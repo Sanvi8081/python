2795f8f73c48e018f2f331d65281efe8765e1804
Sanvi8081 <sanviporwal3101@gmail.com>
2026-02-10 22:02:33 +0530

    Add files via upload

---
+++ string_functions.py
@@ -0,0 +1,18 @@
+#string functions
+#str.endswith()
+str =" i am studying python."
+print(str.endswith("er"))
+#str.capitalize()
+str="this is my day 5 of learning python."
+str=str.capitalize()
+print(str)
+#str.replace()
+str = " this is my day 5 of learning python."
+print(str.replace("python","Javascript"))
+#str.find(word)
+str = " frameworks and libraries."
+print(str.find("l"))
+#str.count()
+str = " this is language is python "
+print(str.count("python"))
+ 
